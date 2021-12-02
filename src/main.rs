637ef3676b10a4b7ebd43d49a529142d3838beba
geno <nullfree.geno@gmail.com>
2021-12-01 21:32:14 -0500

    Loading Elf32/64

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
-use std::io::Read;
 use std::fs::File;
-use std::env;
+use std::io::Read;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 use elf_rs::*;
 
-fn read_elf(filename: &String) {
+mod utils;
+
+/// Terminus is an application for quickly locating exported functions in Elf libraries
+
+#[derive(Debug, StructOpt)]
+pub struct Opt {
+    /// List of function names to search for
+    #[structopt(short, long, default_value = "")]
+    search: Vec<String>,
+
+    /// Directory to search in
+    #[structopt(short, long, default_value = "/lib")]
+    dir: PathBuf,
+}
+
+fn read_elf(filename: &str) {
     let mut elf_file = File::open(filename).unwrap();
     let mut elf_buf = Vec::<u8>::new();
     elf_file.read_to_end(&mut elf_buf).unwrap();
@@ -27,6 +43,10 @@ fn read_elf(filename: &String) {
     }
 }
 
-fn main() {
-    read_elf(&"file.elf".to_string());
+fn main() -> Result<(), Error> {
+    // Process arguments
+    let opt = Opt::from_args();
+    utils::load_dyns(opt)?;
+
+    Ok(())
 }
---
+++ src/utils.rs
@@ -0,0 +1,63 @@
+use crate::*;
+use std::path::Path;
+
+fn get_file_list(opt: &Opt) -> Vec<String> {
+    let mut files = Vec::<String>::new();
+    let item = &opt.dir;
+    if item.is_dir() {
+        let mut tmp = match scope_dir(&item.to_path_buf()) {
+            Ok(t) => t,
+            Err(_) => {
+                println!("Error: Cannot read item: {:?}", item);
+                return files;
+            }
+        };
+        files.append(&mut tmp);
+    } else if item.exists() && item.is_file() {
+        files.push(item.to_str().unwrap().to_string());
+    }
+
+    files
+}
+
+fn scope_dir(dir: &Path) -> Result<Vec<String>, Error> {
+    let path = Path::new(&dir);
+    let mut files = Vec::<String>::new();
+
+    for entry in path.read_dir().unwrap() {
+        if entry.as_ref().unwrap().file_type().unwrap().is_dir() {
+            if entry.as_ref().unwrap().path() == *dir {
+                continue;
+            }
+
+            let mut tmp = match scope_dir(&entry.as_ref().unwrap().path()) {
+                Ok(t) => t,
+                Err(_) => {
+                    println!("Error: Cannot read dir: {:?}", entry);
+                    continue;
+                }
+            };
+            files.append(&mut tmp);
+        } else if entry.as_ref().unwrap().file_type().unwrap().is_file() {
+            files.push(entry.unwrap().path().to_str().unwrap().to_string());
+        }
+    }
+
+    Ok(files)
+}
+
+pub fn load_dyns(opt: Opt) -> Result<(), Error> {
+    let files = get_file_list(&opt);
+
+    if files.is_empty() {
+        println!("[!] No files located");
+        return Ok(());
+    }
+
+    // For each filepath in the input vector...
+    for (_num, item) in files.iter().enumerate() {
+        read_elf(item);
+    }
+
+    Ok(())
+}
