574e36e52086798bc717584d35948c215bd719f4
geno nullfree <nullfree.geno@gmail.com>
2021-12-29 11:27:53 -0500

    Move scan_elf to utils.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use elf_rs::*;
-use r2pipe::R2Pipe;
-use std::fs::File;
-use std::io::Read;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -20,31 +16,10 @@ pub struct Opt {
     dir: PathBuf,
 }
 
-fn read_elf(filename: &str, search: &[String]) -> Result<(), Error> {
-    // TODO: Find better way to validate Elf files
-    let mut elf_file = File::open(filename).unwrap();
-    let mut elf_buf = Vec::<u8>::new();
-    elf_file.read_to_end(&mut elf_buf).unwrap();
-    let _ = Elf::from_bytes(&elf_buf)?;
-
-    // Scan Elf file for exported symbols
-    let mut r2p = R2Pipe::spawn(filename, None).unwrap();
-    r2p.cmd("af").unwrap();
-    let out = r2p.cmd("iE").unwrap();
-
-    for i in search {
-        if out.contains(i) {
-            println!("\r[!] {} exports {}", filename, i);
-        }
-    }
-
-    Ok(())
-}
-
-fn main() -> Result<(), Error> {
+fn main() {
     // Process arguments
     let opt = Opt::from_args();
-    utils::load_dyns(opt)?;
 
-    Ok(())
+    // Scan Elf exports
+    utils::scan_elf_exports(opt);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,11 @@
 use crate::*;
 use crossterm::execute;
 use crossterm::terminal::{Clear, ClearType};
+use elf_rs::*;
+use r2pipe::R2Pipe;
+use std::fs::File;
 use std::io;
+use std::io::Read;
 use std::io::{stdout, Write};
 use std::path::Path;
 
@@ -50,20 +54,44 @@ fn scope_dir(dir: &Path) -> Result<Vec<String>, Error> {
     Ok(files)
 }
 
-pub fn load_dyns(opt: Opt) -> Result<(), Error> {
+pub fn scan_elf_exports(opt: Opt) {
+    // Get all files recursively
     let files = get_file_list(&opt);
 
+    // If no files found, we're done
     if files.is_empty() {
         println!("[!] No files located");
-        return Ok(());
+        return;
     }
 
     // For each filepath in the input vector...
     for (_num, item) in files.iter().enumerate() {
-        if read_elf(item, &opt.search).is_ok() {
+        // Scan the file for exported symbols
+        if scan_elf(item, &opt.search).is_ok() {
+            // Clear the terminal line and print the previously scanned file
             execute!(stdout(), Clear(ClearType::CurrentLine)).unwrap();
-            print!("\r[+] Scanning file: {}", item);
+            print!("\r[+] Scanned file: {}", item);
             io::stdout().flush().unwrap();
+        }
+    }
+}
+
+fn scan_elf(filename: &str, search: &[String]) -> Result<(), Error> {
+    // TODO: Find better way to validate Elf files
+    let mut elf_file = File::open(filename).unwrap();
+    let mut elf_buf = Vec::<u8>::new();
+    elf_file.read_to_end(&mut elf_buf).unwrap();
+    let _ = Elf::from_bytes(&elf_buf)?;
+
+    // Scan Elf file for exported symbols
+    let mut r2p = R2Pipe::spawn(filename, None).unwrap();
+    r2p.cmd("af").unwrap();
+    let out = r2p.cmd("iE").unwrap();
+
+    for i in search {
+        if out.contains(i) {
+            // TODO: Print better info
+            println!("\r[!] {} exports {}", filename, i);
         }
     }
 
